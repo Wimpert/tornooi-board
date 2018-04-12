6aac6a5fb98dee3bffebaf85cfdb5fa996165f47
whv2li8 <wim.holvoet@colruytgroup.com>
2018-04-12 21:43:02 +0200

    some more setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+import { AdminComponent } from './admin/admin.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'admin', component: AdminComponent },
+
+  { path: '**', component: DisplayComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    AdminComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
